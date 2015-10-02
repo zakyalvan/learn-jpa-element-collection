84f2cd131a12e0919cb0d275aca4d2ee3e97351c
zakyalvan <zakyalvan@gmail.com>
2015-10-02 11:00:32 +0700

    Add banner service methods (Create test for them by yourself, please)

--- src/main/java/com/jakartawebs/learn/service/BannerService.java
+++ src/main/java/com/jakartawebs/learn/service/BannerService.java
@@ -3,10 +3,20 @@ package com.jakartawebs.learn.service;
 import java.util.Collection;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.jakartawebs.learn.entity.Banner;
 import com.jakartawebs.learn.entity.Banner.Position;
+import com.mysema.query.types.Predicate;
 
 public interface BannerService {
 	Banner createBanner(Collection<Position> positions);
+	Banner getBanner(Long id);
+	Banner getBanner(Predicate predicate);
 	List<Banner> getBanners();
+	Iterable<Banner> getBanners(Predicate predicate);
+	Page<Banner> getBanners(Pageable pageable, Predicate predicate);
+	void removeBanner(Long id);
+	void removeBanners(Predicate predicate);
 }
--- src/main/java/com/jakartawebs/learn/service/DefaultBannerService.java
+++ src/main/java/com/jakartawebs/learn/service/DefaultBannerService.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jakartawebs.learn.entity.Banner;
 import com.jakartawebs.learn.entity.Banner.Position;
 import com.jakartawebs.learn.entity.BannerRepository;
+import com.mysema.query.types.Predicate;
 
 @Service
 @Transactional(readOnly=true)
@@ -26,9 +29,42 @@ public class DefaultBannerService implements BannerService {
 		banner.setPositions(new ArrayList<>(positions));
 		return bannerRepository.save(banner);
 	}
+	
+	@Override
+	public Banner getBanner(Long id) {
+		return bannerRepository.findOne(id);
+	}
+
+	@Override
+	public Banner getBanner(Predicate predicate) {
+		return bannerRepository.findOne(predicate);
+	}
 
 	@Override
 	public List<Banner> getBanners() {
 		return bannerRepository.findAll();
+	}
+
+	@Override
+	public Iterable<Banner> getBanners(Predicate predicate) {
+		return bannerRepository.findAll(predicate);
+	}
+
+	@Override
+	public Page<Banner> getBanners(Pageable pageable, Predicate predicate) {
+		return bannerRepository.findAll(predicate, pageable);
+	}
+
+	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
+	public void removeBanner(Long id) {
+		bannerRepository.delete(id);
+	}
+
+	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
+	public void removeBanners(Predicate predicate) {
+		Iterable<Banner> banners = bannerRepository.findAll(predicate);
+		bannerRepository.delete(banners);
 	}
 }
